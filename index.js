023d3f20982fd20f455ff6046dc3c340170d86e6
BAaboe <benjamin.mjaatvedt@gmail.com>
2021-11-11 13:21:52 +0100

    stuff happened

---
+++ index.js
@@ -0,0 +1,8 @@
+function searchgBtnClick(){
+    let input = document.getElementById("input").value;
+    console.log(input)
+    window.location.replace(`https://www.google.com/search?q=tea, ${input}`);
+}
+
+
+
