c4f35780e4f451db1f1c483c819ce3ed563cb027
BAaboe <benjamin.mjaatvedt@gmail.com>
2021-11-11 19:07:53 +0100

    bug fix

--- products/index.js
+++ products/index.js
@@ -1,12 +1,12 @@
 function addProducts(){
     console.log("fsafss")
 
-    var products = {0:{"name":"English Breakfast","brand":"Twinigs", "price": "£6.39", "bags": "150"},
-                    1:{"name":"Earl <br> Grey", "brand":"Twinigs", "price": "£6.39", "bags": "150"},
-                    2:{"name":"Lady <br> Grey", "brand":"Twinigs", "price": "£5.29", "bags": "100"},
-                    3:{"name":"English Afternoon", "brand":"Twinigs", "price": "£5.29", "bags": "100"},
-                    4:{"name":"Sweet <br> Chai", "brand":"Yogi Tea", "price": "£5,07", "bags": "17"},
-                    5:{"name":"Bee <br> Happy", "brand":"Yogi Tea", "price": "£4,99", "bags": "17"}};
+    var products = {0:{"name":"English<br>Breakfast","brand":"Twinigs", "price": "£6.39", "bags": "150"},
+                    1:{"name":"Earl<br>Grey", "brand":"Twinigs", "price": "£6.39", "bags": "150"},
+                    2:{"name":"Lady<br>Grey", "brand":"Twinigs", "price": "£5.29", "bags": "100"},
+                    3:{"name":"English<br>Afternoon", "brand":"Twinigs", "price": "£5.29", "bags": "100"},
+                    4:{"name":"Sweet<br>Chai", "brand":"Yogi Tea", "price": "£5,07", "bags": "17"},
+                    5:{"name":"Bee<br>Happy", "brand":"Yogi Tea", "price": "£4,99", "bags": "17"}};
     console.log("sadsadsa")
 
     /*fetch("../assets/products.json")
@@ -19,8 +19,8 @@ function addProducts(){
         completelist.innerHTML += `<li class="productItem">
                                     <h3>${products[i]["name"]}</h3>
                                     <h4>Brand: ${products[i]["brand"]}</h4>
-                                    <p>Bags: ${products[i]["bags"]}</p>
-                                    <p>${products[i]["price"]}</p>
+                                    <p>Tea bags: ${products[i]["bags"]}</p>
+                                    <p>Price: ${products[i]["price"]}</p>
                                     <a href="temp.html?name=${i}" id="productBtn">View Product</a>
                                     </li>`;
     }
